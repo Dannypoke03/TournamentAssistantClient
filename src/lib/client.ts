426274a518855c7abe929c857dd20a1492b59b03
Dannypoke03 <daniel.carruthers@outlook.com>
2022-05-02 23:24:31 +1000

    update packet handling

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -35,98 +35,57 @@ export class Client {
     public handlePacket(packet: Packet.Packet) {
         if (!this.isConnected) return;
         this._event.emit("packet", packet);
-        console.log(packet.packet);
-        // switch (packet.Type) {
-        //     case PacketType.ConnectResponse:
-        //         break;
-        //     case PacketType.Event:
-        //         const event: Event = packet.SpecificPacket;
-        //         switch (event.Type) {
-        //             case EventType.CoordinatorAdded:
-        //                 this.coordinatorAdded(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.CoordinatorLeft:
-        //                 this.coordinatorLeft(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.MatchCreated:
-        //                 this.matchCreated(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.MatchUpdated:
-        //                 this.matchUpdated(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.MatchDeleted:
-        //                 this.matchDeleted(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.PlayerAdded:
-        //                 this.playerAdded(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.PlayerUpdated:
-        //                 this.playerUpdated(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.PlayerLeft:
-        //                 this.playerLeft(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.QualifierEventCreated:
-        //                 this.qualifierEventCreated(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.QualifierEventUpdated:
-        //                 this.qualifierEventUpdated(event.ChangedObject, packet.From);
-        //                 break;
-        //             case EventType.QualifierEventDeleted:
-        //                 this.qualifierEventDeleted(event.ChangedObject, packet.From);
-        //                 break;
-        //             default:
-        //                 break;
-        //         }
-        //         break;
-        //     case PacketType.ForwardingPacket:
-        //         packet.SpecificPacket.From = packet.From;
-        //         // this.handlePacket(packet.SpecificPacket);
-        //         this._event.emit('forwardingPacket', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.Acknowledgement:
-        //         this._event.emit('acknowledgement', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.Command:
-        //         this._event.emit('command', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.Connect:
-        //         this._event.emit('connect', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.File:
-        //         this._event.emit('file', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.LoadedSong:
-        //         this._event.emit('loadedSong', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.LoadSong:
-        //         this._event.emit('loadSong', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.Response:
-        //         this._event.emit('response', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.ScoreRequest:
-        //         this._event.emit('scoreRequest', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.ScoreRequestResponse:
-        //         this._event.emit('scoreRequestResponse', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.SongFinished:
-        //         this._event.emit('songFinished', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.SongList:
-        //         this._event.emit('songList', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.SubmitScore:
-        //         this._event.emit('submitScore', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.Message:
-        //         this._event.emit('message', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        //     case PacketType.MessageResponse:
-        //         this._event.emit('messageResponse', { from: packet.From, data: packet.SpecificPacket });
-        //         break;
-        // }
+        if (packet.event) {
+            const event = packet.event;
+            if (event.coordinator_added_event) {
+                this.coordinatorAdded(event.coordinator_added_event.coordinator, packet.from);
+            } else if (event.coordinator_left_event) {
+                this.coordinatorLeft(event.coordinator_left_event.coordinator, packet.from);
+            } else if (event.match_created_event) {
+                this.matchCreated(event.match_created_event.match, packet.from);
+            } else if (event.match_updated_event) {
+                this.matchUpdated(event.match_updated_event.match, packet.from);
+            } else if (event.match_deleted_event) {
+                this.matchDeleted(event.match_deleted_event.match, packet.from);
+            } else if (event.player_added_event) {
+                this.playerAdded(event.player_added_event.player, packet.from);
+            } else if (event.player_updated_event) {
+                this.playerUpdated(event.player_updated_event.player, packet.from);
+            } else if (event.player_left_event) {
+                this.playerLeft(event.player_left_event.player, packet.from);
+            } else if (event.qualifier_created_event) {
+                this.qualifierEventCreated(event.qualifier_created_event.event, packet.from);
+            } else if (event.qualifier_updated_event) {
+                this.qualifierEventUpdated(event.qualifier_updated_event.event, packet.from);
+            } else if (event.qualifier_deleted_event) {
+                this.qualifierEventDeleted(event.qualifier_deleted_event.event, packet.from);
+            }
+        } else if (packet.forwarding_packet) {
+            packet.forwarding_packet.packet.from = packet.from;
+            this._event.emit("forwardingPacket", { from: packet.from, data: packet.forwarding_packet });
+        } else if (packet.acknowledgement) {
+            this._event.emit("acknowledgement", { from: packet.from, data: packet.acknowledgement });
+        } else if (packet.command) {
+            this._event.emit("command", { from: packet.from, data: packet.command });
+        } else if (packet.connect) {
+            this._event.emit("connect", { from: packet.from, data: packet.connect });
+        } else if (packet.file) {
+            this._event.emit("file", { from: packet.from, data: packet.file });
+        } else if (packet.loaded_song) {
+            this._event.emit("loadedSong", { from: packet.from, data: packet.loaded_song });
+        } else if (packet.load_song) {
+            this._event.emit("loadSong", { from: packet.from, data: packet.load_song });
+        } else if (packet.response) {
+            this._event.emit("response", { from: packet.from, data: packet.response });
+        } else if (packet.score_request) {
+            this._event.emit("scoreRequest", { from: packet.from, data: packet.score_request });
+        } else if (packet.score_request_response) {
+            this._event.emit("scoreRequestResponse", { from: packet.from, data: packet.score_request_response });
+        } else if (packet.song_finished) {
+            this._event.emit("songFinished", { from: packet.from, data: packet.song_finished });
+        } else if (packet.submit_score) {
+            this._event.emit("submitScore", { from: packet.from, data: packet.submit_score });
+        }
     }
 
     coordinatorAdded(data: Models.Coordinator, from: string) {
