ac9af49429d49e8d354bde0addf3c5fa81e751dd
Dannypoke03 <daniel.carruthers@outlook.com>
2023-04-17 17:53:24 -1000

    update example testing script

--- scripts/exampleTest.sh
+++ scripts/exampleTest.sh
@@ -5,10 +5,16 @@ cd "$parent_path/../examples"
 
 for dir in $(ls -1 | sort -r)*
 do
-  echo "Testing $dir"
+  echo -ne "Testing: $dir... "\\r
   rm -rf ../examples/$dir/node_modules
   cd "$dir"
-  npm install
-  npm run test
+  npm install &> /dev/null
+  if ! npm run test &> /dev/null
+  then
+    echo "Testing: $dir... [FAIL]"
+    exit 1
+  else
+    echo "Testing: $dir... [OK]"
+  fi
   cd ..
 done
